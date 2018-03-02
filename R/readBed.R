985cb3f8ecbac84052e9a72d1294df5bda50f546
Fabian <fabian@dhcp563-165.laptop-wlc.uni-heidelberg.de>
2018-03-02 18:39:53 +0100

    update

--- R/readBed.R
+++ R/readBed.R
@@ -10,17 +10,17 @@ readBed<-function(filename, chr = 1, ignore_strand = FALSE) {
   # check input arguments
   if(!file.exists(filename)) stop("file not found")
   if(!is.numeric(chr)) stop("chr must be a number")
-  
+
   # read data
   data <- data.table::fread(filename, data.table=FALSE, header=FALSE)
-  
+
   # assign column names for conversion to GRanges object
-  colnames(data) <- c("chr", "start", "end", "strand")
+  colnames(data) <- c("chr", "start", "end", "name", "score", "strand")
 
   # convert data table to GRanges object
   reads <- GenomicRanges::makeGRangesFromDataFrame(data, keep.extra.columns=FALSE, ignore.strand=ignore_strand, seqinfo=NULL,
        seqnames.field="chr", start.field="start", end.field="end", strand.field="strand", starts.in.df.are.0based=FALSE)
-  
+
   # return data
   return(reads)
 }
