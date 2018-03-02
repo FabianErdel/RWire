ba5d8b2ddca698cd3c543d8ae839a4c1e880462c
Fabian <fabian@dhcp563-165.laptop-wlc.uni-heidelberg.de>
2018-03-02 18:06:00 +0100

    update

--- sample-scripts/make_accessibility_matrix.R
+++ sample-scripts/make_accessibility_matrix.R
@@ -1,11 +1,11 @@
-# make tiles for chromosome 18, using default settings
-tiles <- makeTiles(18)
+# make tiles for chromosome 1 (using default tile size)
+tiles <- makeTiles(1)
 
 # make accessibility matrix
 am <- makeAccMatrix("your_path", tiles)
 
 # save accessibility matrix
-write.table(am, file = “your_path/am", sep = "\t", row.names = FALSE, quote = FALSE)
+write.table(am, file = “your_path2", sep = "\t", row.names = FALSE, quote = FALSE)
 
 # make histogram for integrations per cell and integrations per region
 hist_ipc <- makeIPCHist(am)
