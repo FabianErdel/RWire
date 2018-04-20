057517b5a6ac60680cfa5dd86afd953807f7a6dc
Fabian <fabian@Fabians-MacBook-Pro.local>
2018-04-20 17:17:25 +0200

    update

--- sample-scripts/compare_correlation_matrices.R
+++ sample-scripts/compare_correlation_matrices.R
@@ -5,12 +5,12 @@ outpath <- "your_path_for_matrix"
 
 
 # read accessibility matrices (containing data for the same genomic regions, but for different conditions)
-am1 <- fread(inpath1, data.table=FALSE, sep="\t")
-am2 <- fread(inpath2, data.table=FALSE, sep="\t")
+ram1 <- fread(inpath1, data.table=FALSE, sep="\t")
+ram2 <- fread(inpath2, data.table=FALSE, sep="\t")
 
 # remove empty rows
-am1 <- am1[rowSums(am1[,4:dim(am1)[2]])>0, ]
-am2 <- am2[rowSums(am2[,4:dim(am2)[2]])>0, ]
+am1 <- ram1[rowSums(ram1[,4:dim(ram1)[2]])>0 | rowSums(ram2[,4:dim(ram2)[2]])>0, ]
+am2 <- ram2[rowSums(ram1[,4:dim(ram1)[2]])>0 | rowSums(ram2[,4:dim(ram2)[2]])>0, ]
 
 # select genomic region of interest
 start <- 1
@@ -21,7 +21,7 @@ am1 <- am1[am1[,2]>start & am1[,3]<end, ]
 am2 <- am2[am2[,2]>start & am2[,3]<end, ]
 
 # get number of accessible regions
-nrois <- dim(am1)[1]-3
+nrois <- dim(am1)[1]
 
 # get bootstrapped correlation coefficients
 replicates <- 100
