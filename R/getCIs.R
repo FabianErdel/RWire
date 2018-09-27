b4cbead8c6cfc97faeaf330b7271a2c3a8492c7f
Fabian <fabian@Fabians-MacBook-Pro.local>
2018-09-28 00:25:53 +0200

    update

--- R/getCIs.R
+++ R/getCIs.R
@@ -9,23 +9,16 @@ getCIs<-function(corrng, confidence = 0.95) {
   # check input arguments
   if(!is.list(corrng)) stop("corrng must be a list")
 
-  # retrieve items from corrng list
-  chr <- corrng[[1]]
-  start <- corrng[[2]]
-  end <- corrng[[3]]
-  cortbl <- corrng[[4]]
-
-
   # retrieve start/end of confidence intervals and median correlation coefficient
-  replicates <- dim(cortbl)[3]
+  replicates <- dim(corrng[[4]])[3]
   tail <- round(0.5*(1-confidence)*replicates)
   start_index <- tail
   end_index <- replicates - tail + 2
 
-  res1 <- new("CorMatrix", coord1 = cbind(chr,start,end), coord2 = cbind(chr,start,end), cormat = corrng[[5]])
-  res2 <- new("CorMatrix", coord1 = cbind(chr,start,end), coord2 = cbind(chr,start,end), cormat = as.data.frame(cortbl[, , start_index]))
-  res3 <- new("CorMatrix", coord1 = cbind(chr,start,end), coord2 = cbind(chr,start,end), cormat = as.data.frame(cortbl[, , end_index]))
-  res4 <- new("CorMatrix", coord1 = cbind(chr,start,end), coord2 = cbind(chr,start,end), cormat = as.data.frame(cortbl[, , 1+replicates/2]))
+  res1 <- new("CorMatrix", coord1 = data.frame(corrng[[1]],corrng[[2]],corrng[[3]]), coord2 = data.frame(corrng[[1]],corrng[[2]],corrng[[3]]), cormat = corrng[[5]])
+  res2 <- new("CorMatrix", coord1 = data.frame(corrng[[1]],corrng[[2]],corrng[[3]]), coord2 = data.frame(corrng[[1]],corrng[[2]],corrng[[3]]), cormat = as.data.frame(cortbl[, , start_index]))
+  res3 <- new("CorMatrix", coord1 = data.frame(corrng[[1]],corrng[[2]],corrng[[3]]), coord2 = data.frame(corrng[[1]],corrng[[2]],corrng[[3]]), cormat = as.data.frame(cortbl[, , end_index]))
+  res4 <- new("CorMatrix", coord1 = data.frame(chr,start,end), coord2 = data.frame(chr,start,end), cormat = as.data.frame(cortbl[, , 1+replicates/2]))
 
   # return correlation coefficients and start/end positions of confidence intervals
   return(list(res1,res2,res3,res4))
