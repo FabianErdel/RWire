3ab70aea21d7d3889156cf9c93f125450d20f529
Fabian <fabian@dhcp565-090.laptop-wlc.uni-heidelberg.de>
2018-09-27 17:50:59 +0200

    update

--- R/scaleCorMatrix.R
+++ R/scaleCorMatrix.R
@@ -1,6 +1,6 @@
 #' This function scales correlation matrices to genomic coordinates
 #'
-#' @param cormat Correlation matrix (data frame)
+#' @param cm Correlation matrix
 #' @param chr Number of chromosome
 #' @param size Number of genomic regions to be used
 #' @param start Minimum genomic coordinate
@@ -8,20 +8,21 @@
 #' @return Scaled correlation matrix
 #' @export
 
-scaleCorMatrix<-function(cormat, chr, size = 1000, start = 0, end = 249250621) {
+scaleCorMatrix<-function(cm, chr, size = 1000, start = 0, end = 249250621) {
   # check input arguments
-  if(!is.data.frame(cormat)) stop("cormat must be a data frame")
+  if(class(cm)!="CorMatrix") stop("cm must be a CorMatrix object")
+  if(!identical(cm@coord1,cm@coord2)) stop("cm must be an autocorrelation matrix")
   if(!is.numeric(chr)) stop("chr must be a number")
   if(!is.numeric(size)) stop("size must be a number")
   if(!is.numeric(start)) stop("start must be a number")
   if(!is.numeric(end)) stop("end must be a number")
 
   # determine number of ROIs
-  nrois <- dim(cormat)[1]
+  nrois <- dim(cm@cormat)[1]
 
   # make numeric correlation matrix containing regions (on chromosome chr) between start and end
-  sel_indices <- cormat[,1]==paste0('chr',chr) & cormat[,2]>start & cormat[,3]<end
-  m <- data.matrix(cormat[sel_indices, c(FALSE, TRUE, TRUE, sel_indices)])
+  sel_indices <- cm@coord1[,1]==paste0('chr',chr) & cm@coord1[,2]>start & cm@coord1[,3]<end
+  m <- data.matrix(cm@cormat[sel_indices, sel_indices])
 
   # determine scaling factor (kb/pixel)
   # scale <- (max(m[,1])-min(m[,1]))/size
