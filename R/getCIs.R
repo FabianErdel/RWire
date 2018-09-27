3305b03d91d6af4f18a4afe30ac0e11bc3b2997a
Fabian <fabian@Fabians-MacBook-Pro.local>
2018-09-28 00:20:14 +0200

    update

--- R/getCIs.R
+++ R/getCIs.R
@@ -2,7 +2,7 @@
 #'
 #' @param corrng Table with annotations and correlation coefficients
 #' @param confidence Confidence level (between 0 and 1)
-#' @return Stack of matrices containing correlation coefficients (first layer) along with minimum, maximum and median values from bootstrapping (second, third and fourth layer)
+#' @return Stack of CorMatrices containing correlation coefficients (first layer) along with minimum, maximum and median values from bootstrapping (second, third and fourth layer)
 #' @export
 
 getCIs<-function(corrng, confidence = 0.95) {
@@ -14,7 +14,7 @@ getCIs<-function(corrng, confidence = 0.95) {
   start <- corrng[[2]]
   end <- corrng[[3]]
   cortbl <- corrng[[4]]
-  res1 <- corrng[[5]]
+
 
   # retrieve start/end of confidence intervals and median correlation coefficient
   replicates <- dim(cortbl)[3]
@@ -22,15 +22,10 @@ getCIs<-function(corrng, confidence = 0.95) {
   start_index <- tail
   end_index <- replicates - tail + 2
 
-  res2 <- as.data.frame(cortbl[, , start_index])
-  res3 <- as.data.frame(cortbl[, , end_index])
-  res4 <- as.data.frame(cortbl[, , 1+replicates/2])
-
-  # add annotation columns
-  res1 <- cbind(chr, start, end, res1) # correlation coefficient without bootstrapping
-  res2 <- cbind(chr, start, end, res2) # minimum correlation coefficient from bootstrapping
-  res3 <- cbind(chr, start, end, res3) # maximum correlation coefficient from bootstrapping
-  res4 <- cbind(chr, start, end, res4) # median correlation coefficient from bootstrapping
+  res1 <- new("CorMatrix", coord1 = cbind(chr,start,end), coord2 = cbind(chr,start,end), cormat = corrng[[5]])
+  res2 <- new("CorMatrix", coord1 = cbind(chr,start,end), coord2 = cbind(chr,start,end), cormat = as.data.frame(cortbl[, , start_index]))
+  res3 <- new("CorMatrix", coord1 = cbind(chr,start,end), coord2 = cbind(chr,start,end), cormat = as.data.frame(cortbl[, , end_index]))
+  res4 <- new("CorMatrix", coord1 = cbind(chr,start,end), coord2 = cbind(chr,start,end), cormat = as.data.frame(cortbl[, , 1+replicates/2]))
 
   # return correlation coefficients and start/end positions of confidence intervals
   return(list(res1,res2,res3,res4))
